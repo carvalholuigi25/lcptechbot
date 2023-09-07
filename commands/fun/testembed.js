fd50485aec1dc713dacb503e72b45b5decc6a8e0
Luigi Carvalho <carvalholuigi25@gmail.com>
2023-09-07 15:27:01 +0100

    added createMyEmbed in embeds.js and some changes in commands

--- commands/fun/testembed.js
+++ commands/fun/testembed.js
@@ -1,11 +1,59 @@
 const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
-const { exampleEmbed } = require('../../features/embeds/embeds.js');
+const { createMyEmbed } = require('../../features/embeds/embeds');
 
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('testembed')
 		.setDescription('This cmd will test the embed if it works!'),
 	async execute(interaction) {
+		const exampleEmbed = createMyEmbed({
+			color: 0x0099ff,
+			title: 'Testing embed...',
+			url: 'https://github.com/carvalholuigi25/lcptechbot',
+			author: {
+				name: 'Luis Carvalho',
+				icon_url: 'https://dub01pap002files.storage.live.com/y4mTK9iStdQdh2ZzuaR6Fl3ZFEQjiNudFd0d2EKFLZmF6eqaxmG9JAiRIJAOdM4uvg1K9fD5btgk2M3rcG-msJdaV-tj9sTdxS8gdPVLfXwdZ8tCzVxQbAinvyZ-1LMqhdnlvZg2rEC9NKEFrraEHtTKUD9QcpBqVd_WmLlZIUUbs4sDYhXvr3n-YrdXx_AQpagDqgFzzHt5yZRpcB1vi1ACnIuUOlcMne1b8T8spViIIU?encodeFailures=1&width=500&height=500',
+				url: 'https://github.com/carvalholuigi25/lcptechbot',
+			},
+			description: 'This is a test of embed post!',
+			thumbnail: {
+				url: 'https://dub01pap002files.storage.live.com/y4mTK9iStdQdh2ZzuaR6Fl3ZFEQjiNudFd0d2EKFLZmF6eqaxmG9JAiRIJAOdM4uvg1K9fD5btgk2M3rcG-msJdaV-tj9sTdxS8gdPVLfXwdZ8tCzVxQbAinvyZ-1LMqhdnlvZg2rEC9NKEFrraEHtTKUD9QcpBqVd_WmLlZIUUbs4sDYhXvr3n-YrdXx_AQpagDqgFzzHt5yZRpcB1vi1ACnIuUOlcMne1b8T8spViIIU?encodeFailures=1&width=500&height=500',
+			},
+			fields: [
+				{
+					name: 'Regular field title',
+					value: 'Some value here',
+				},
+				{
+					name: '\u200b',
+					value: '\u200b',
+					inline: false,
+				},
+				{
+					name: 'Inline field title',
+					value: 'Some value here',
+					inline: true,
+				},
+				{
+					name: 'Inline field title',
+					value: 'Some value here',
+					inline: true,
+				},
+				{
+					name: 'Inline field title',
+					value: 'Some value here',
+					inline: true,
+				},
+			],
+			image: {
+				url: 'https://dub01pap002files.storage.live.com/y4mTK9iStdQdh2ZzuaR6Fl3ZFEQjiNudFd0d2EKFLZmF6eqaxmG9JAiRIJAOdM4uvg1K9fD5btgk2M3rcG-msJdaV-tj9sTdxS8gdPVLfXwdZ8tCzVxQbAinvyZ-1LMqhdnlvZg2rEC9NKEFrraEHtTKUD9QcpBqVd_WmLlZIUUbs4sDYhXvr3n-YrdXx_AQpagDqgFzzHt5yZRpcB1vi1ACnIuUOlcMne1b8T8spViIIU?encodeFailures=1&width=500&height=500',
+			},
+			timestamp: new Date().toISOString(),
+			footer: {
+				text: 'By Luis Carvalho',
+				icon_url: 'https://dub01pap002files.storage.live.com/y4mTK9iStdQdh2ZzuaR6Fl3ZFEQjiNudFd0d2EKFLZmF6eqaxmG9JAiRIJAOdM4uvg1K9fD5btgk2M3rcG-msJdaV-tj9sTdxS8gdPVLfXwdZ8tCzVxQbAinvyZ-1LMqhdnlvZg2rEC9NKEFrraEHtTKUD9QcpBqVd_WmLlZIUUbs4sDYhXvr3n-YrdXx_AQpagDqgFzzHt5yZRpcB1vi1ACnIuUOlcMne1b8T8spViIIU?encodeFailures=1&width=500&height=500',
+			}
+		});
 		return interaction.reply({ embeds: [exampleEmbed] });
 	},
 };
--- commands/general/help.js
+++ commands/general/help.js
@@ -1,18 +1,24 @@
 const { SlashCommandBuilder } = require('discord.js');
+const { createMyEmbed } = require('../../features/embeds/embeds');
 
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('help')
 		.setDescription('Shows the list of all commands.'),
 	async execute(interaction) {
-		return interaction.reply(`
-        Lista de comandos:\n\n
-        /help --> Isto mostra a lista de todos comandos.\n
-        /welcome --> Isto mostra a mensagem de boas-vindas.\n
-        /rules --> Isto mostra a mensagem sobre as regras do servidor.\n
-        /time --> Isto mostra a mensagem sobre a data e a hora atual do seu fuso horário.\n
-        /testembed --> Isto mostra a mensagem sobre o teste de embed.\n
-        /ping --> Isto mostra a mensagem sobre ping e irá responder pong.\n
-        /achievements --> Isto mostra a mensagem sobre o meu link de retro achievements.`);
+        const helpEmB = createMyEmbed({
+            title: 'Help',
+            description: `
+            Lista de comandos:\n\n
+            /help --> Isto mostra a lista de todos comandos.\n
+            /welcome --> Isto mostra a mensagem de boas-vindas.\n
+            /rules --> Isto mostra a mensagem sobre as regras do servidor.\n
+            /time --> Isto mostra a mensagem sobre a data e a hora atual do seu fuso horário.\n
+            /testembed --> Isto mostra a mensagem sobre o teste de embed.\n
+            /ping --> Isto mostra a mensagem sobre ping e irá responder pong.\n
+            /achievements --> Isto mostra a mensagem sobre o meu link de retro achievements.`,
+            fields: []
+        })
+		return interaction.reply({ embeds: [helpEmB] });
 	},
 };
