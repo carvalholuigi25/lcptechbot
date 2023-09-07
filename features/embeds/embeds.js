f972c5f9504d7767fc0e1799412001e2d5a0f570
Luigi Carvalho <carvalholuigi25@gmail.com>
2023-09-07 15:27:01 +0100

    added createMyEmbed in embeds.js and made some changes in commands / models

--- features/embeds/embeds.js
+++ features/embeds/embeds.js
@@ -1,50 +1,52 @@
-const exampleEmbed = {
-    color: 0x0099ff,
-    title: 'Some title',
-    url: 'https://discord.js.org',
-    author: {
-        name: 'Some name',
-        icon_url: 'https://dub01pap002files.storage.live.com/y4mTK9iStdQdh2ZzuaR6Fl3ZFEQjiNudFd0d2EKFLZmF6eqaxmG9JAiRIJAOdM4uvg1K9fD5btgk2M3rcG-msJdaV-tj9sTdxS8gdPVLfXwdZ8tCzVxQbAinvyZ-1LMqhdnlvZg2rEC9NKEFrraEHtTKUD9QcpBqVd_WmLlZIUUbs4sDYhXvr3n-YrdXx_AQpagDqgFzzHt5yZRpcB1vi1ACnIuUOlcMne1b8T8spViIIU?encodeFailures=1&width=500&height=500',
-        url: 'https://discord.js.org',
-    },
-    description: 'Some description here',
-    thumbnail: {
-        url: 'https://dub01pap002files.storage.live.com/y4mTK9iStdQdh2ZzuaR6Fl3ZFEQjiNudFd0d2EKFLZmF6eqaxmG9JAiRIJAOdM4uvg1K9fD5btgk2M3rcG-msJdaV-tj9sTdxS8gdPVLfXwdZ8tCzVxQbAinvyZ-1LMqhdnlvZg2rEC9NKEFrraEHtTKUD9QcpBqVd_WmLlZIUUbs4sDYhXvr3n-YrdXx_AQpagDqgFzzHt5yZRpcB1vi1ACnIuUOlcMne1b8T8spViIIU?encodeFailures=1&width=500&height=500',
-    },
-    fields: [
-        {
-            name: 'Regular field title',
-            value: 'Some value here',
+function createMyEmbed(emb) {
+    return {
+        color: emb.color ?? 0x0099ff,
+        title: emb.title ?? '',
+        url: emb.url ?? 'https://discord.js.org',
+        author: emb.author ?? {
+            name: '',
+            icon_url: 'https://dub01pap002files.storage.live.com/y4mTK9iStdQdh2ZzuaR6Fl3ZFEQjiNudFd0d2EKFLZmF6eqaxmG9JAiRIJAOdM4uvg1K9fD5btgk2M3rcG-msJdaV-tj9sTdxS8gdPVLfXwdZ8tCzVxQbAinvyZ-1LMqhdnlvZg2rEC9NKEFrraEHtTKUD9QcpBqVd_WmLlZIUUbs4sDYhXvr3n-YrdXx_AQpagDqgFzzHt5yZRpcB1vi1ACnIuUOlcMne1b8T8spViIIU?encodeFailures=1&width=500&height=500',
+            url: 'https://discord.js.org',
         },
-        {
-            name: '\u200b',
-            value: '\u200b',
-            inline: false,
+        description: emb.description ?? '',
+        thumbnail: emb.thumbnail ?? {
+            url: 'https://dub01pap002files.storage.live.com/y4mTK9iStdQdh2ZzuaR6Fl3ZFEQjiNudFd0d2EKFLZmF6eqaxmG9JAiRIJAOdM4uvg1K9fD5btgk2M3rcG-msJdaV-tj9sTdxS8gdPVLfXwdZ8tCzVxQbAinvyZ-1LMqhdnlvZg2rEC9NKEFrraEHtTKUD9QcpBqVd_WmLlZIUUbs4sDYhXvr3n-YrdXx_AQpagDqgFzzHt5yZRpcB1vi1ACnIuUOlcMne1b8T8spViIIU?encodeFailures=1&width=500&height=500',
         },
-        {
-            name: 'Inline field title',
-            value: 'Some value here',
-            inline: true,
+        fields: emb.fields ?? [
+            {
+                name: 'Regular field title',
+                value: 'Some value here',
+            },
+            {
+                name: '\u200b',
+                value: '\u200b',
+                inline: false,
+            },
+            {
+                name: 'Inline field title',
+                value: 'Some value here',
+                inline: true,
+            },
+            {
+                name: 'Inline field title',
+                value: 'Some value here',
+                inline: true,
+            },
+            {
+                name: 'Inline field title',
+                value: 'Some value here',
+                inline: true,
+            },
+        ],
+        image: emb.images ?? {
+            url: 'https://dub01pap002files.storage.live.com/y4mTK9iStdQdh2ZzuaR6Fl3ZFEQjiNudFd0d2EKFLZmF6eqaxmG9JAiRIJAOdM4uvg1K9fD5btgk2M3rcG-msJdaV-tj9sTdxS8gdPVLfXwdZ8tCzVxQbAinvyZ-1LMqhdnlvZg2rEC9NKEFrraEHtTKUD9QcpBqVd_WmLlZIUUbs4sDYhXvr3n-YrdXx_AQpagDqgFzzHt5yZRpcB1vi1ACnIuUOlcMne1b8T8spViIIU?encodeFailures=1&width=500&height=500',
         },
-        {
-            name: 'Inline field title',
-            value: 'Some value here',
-            inline: true,
-        },
-        {
-            name: 'Inline field title',
-            value: 'Some value here',
-            inline: true,
-        },
-    ],
-    image: {
-        url: 'https://dub01pap002files.storage.live.com/y4mTK9iStdQdh2ZzuaR6Fl3ZFEQjiNudFd0d2EKFLZmF6eqaxmG9JAiRIJAOdM4uvg1K9fD5btgk2M3rcG-msJdaV-tj9sTdxS8gdPVLfXwdZ8tCzVxQbAinvyZ-1LMqhdnlvZg2rEC9NKEFrraEHtTKUD9QcpBqVd_WmLlZIUUbs4sDYhXvr3n-YrdXx_AQpagDqgFzzHt5yZRpcB1vi1ACnIuUOlcMne1b8T8spViIIU?encodeFailures=1&width=500&height=500',
-    },
-    timestamp: new Date().toISOString(),
-    footer: {
-        text: 'Some footer text here',
-        icon_url: 'https://dub01pap002files.storage.live.com/y4mTK9iStdQdh2ZzuaR6Fl3ZFEQjiNudFd0d2EKFLZmF6eqaxmG9JAiRIJAOdM4uvg1K9fD5btgk2M3rcG-msJdaV-tj9sTdxS8gdPVLfXwdZ8tCzVxQbAinvyZ-1LMqhdnlvZg2rEC9NKEFrraEHtTKUD9QcpBqVd_WmLlZIUUbs4sDYhXvr3n-YrdXx_AQpagDqgFzzHt5yZRpcB1vi1ACnIuUOlcMne1b8T8spViIIU?encodeFailures=1&width=500&height=500',
-    }
-};
+        timestamp: emb.timestamp ?? new Date().toISOString(),
+        footer: emb.footer ?? {
+            text: '',
+            icon_url: 'https://dub01pap002files.storage.live.com/y4mTK9iStdQdh2ZzuaR6Fl3ZFEQjiNudFd0d2EKFLZmF6eqaxmG9JAiRIJAOdM4uvg1K9fD5btgk2M3rcG-msJdaV-tj9sTdxS8gdPVLfXwdZ8tCzVxQbAinvyZ-1LMqhdnlvZg2rEC9NKEFrraEHtTKUD9QcpBqVd_WmLlZIUUbs4sDYhXvr3n-YrdXx_AQpagDqgFzzHt5yZRpcB1vi1ACnIuUOlcMne1b8T8spViIIU?encodeFailures=1&width=500&height=500',
+        }
+    };
+}
 
-module.exports = { exampleEmbed };
+module.exports = { createMyEmbed };
---
+++ models/embeds.ts
@@ -0,0 +1,39 @@
+// src: https://old.discordjs.dev/#/docs/discord.js/14.13.0/class/Embed
+
+export interface MyEmbeds {
+    color: string;
+    title: string;
+    url?: string;
+    author?: MyEmbedsAuthors;
+    description?: string;
+    thumbnail?: MyEmbedsThumbnails;
+    fields?: MyEmbedsFields[];
+    images?: MyEmbedsImages;
+    timestamp?: string | Date;
+    footer?: MyEmbedsFooter;
+}
+
+export interface MyEmbedsAuthors {
+    name?: string;
+    icon_url?: string;
+    url?: string;
+}
+
+export interface MyEmbedsThumbnails {
+    url?: string;
+}
+
+export interface MyEmbedsFields {
+    name?: string;
+    value?: string;
+    inline?: boolean;
+}
+
+export interface MyEmbedsImages {
+    url?: string;
+}
+
+export interface MyEmbedsFooter {
+    text?: string;
+    icon_url?: string;
+}
