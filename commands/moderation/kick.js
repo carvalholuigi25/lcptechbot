bf29f599a0f7a650466c676f0e7ac8c9967d5e66
Luigi Carvalho <carvalholuigi25@gmail.com>
2023-09-08 15:34:23 +0100

    added more commands for moderation

--- commands/moderation/kick.js
+++ commands/moderation/kick.js
@@ -3,10 +3,31 @@ const { SlashCommandBuilder } = require('discord.js');
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('kick')
-		.setDescription('Select a member and kick them (but not really).')
-		.addUserOption(option => option.setName('target').setDescription('The member to kick').setRequired(true)),
+		.setDescription('Select a member and kick them.')
+		.addUserOption(option => option.setName('target').setDescription('The member to kick').setRequired(true))
+		.addStringOption(option => option.setName('reason').setDescription('The reason of kick member(s)').setRequired(false)),
 	async execute(interaction) {
-		const member = interaction.options.getMember('target');
-		return interaction.reply({ content: `You wanted to kick: ${member.user.username}`, ephemeral: true });
+		const isThisCmdEnabled = false;
+
+        if(!isThisCmdEnabled) return interaction.reply({ content: "This cmd is disabled by mods/admins.", ephemeral: true });
+        if(!interaction.member.hasPermission("KICK_MEMBERS")) return interaction.reply({ content: "You cant use that!", ephemeral: true })
+        if(!interaction.guild.me.hasPermission("KICK_MEMBERS")) return interaction.reply({ content: 'I dont have the right permissions.', ephemeral: true })
+
+		const member = interaction.mentions.members.first() || interaction.options.getMember('target');
+		const reason = interaction.options.getString('reason');
+
+        if(!member) return interaction.reply({ content: 'Cant seem to find this user. Sorry about that :/', ephemeral: true });
+        if(!member.kicknable) return interaction.reply({ content: 'This user cant be kicked. It is either because they are a mod/admin, or their highest role is higher than mine', ephemeral: true });
+		if(member.user.username === interaction.user.username) return interaction.reply({ content: "You cannot kick yourself!", ephemeral: true });
+
+        member.kick(`${reason}`).catch(error => {
+			console.error(error);
+			interaction.reply({ content: 'There was an error trying to kick this member!', ephemeral: true });
+		});
+
+		return interaction.reply({ 
+			content: `The member ${member.user.username} has been kicked by ${interaction.user.username} (Reason: ${reason})`, 
+			ephemeral: true 
+		});
 	},
 };
---
+++ commands/moderation/unban.js
@@ -0,0 +1,33 @@
+const { SlashCommandBuilder } = require('discord.js');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('unban')
+		.setDescription('Select a member and unban them.')
+		.addUserOption(option => option.setName('target').setDescription('The member to unban').setRequired(true))
+		.addStringOption(option => option.setName('reason').setDescription('The reason of unban member(s)').setRequired(false)),
+	async execute(interaction) {
+		const isThisCmdEnabled = false;
+
+        if(!isThisCmdEnabled) return interaction.reply({ content: "This cmd is disabled by mods/admins.", ephemeral: true });
+        if(!interaction.member.hasPermission("UNBAN_MEMBERS")) return interaction.reply({ content: "You cant use that!", ephemeral: true });
+        if(!interaction.guild.me.hasPermission("UNBAN_MEMBERS")) return interaction.reply({ content: 'I dont have the right permissions.', ephemeral: true });
+
+		const member = interaction.mentions.members.first() || interaction.options.getMember('target');
+		const reason = interaction.options.getString('reason');
+
+        if(!member) return interaction.reply({ content: 'Cant seem to find this user. Sorry about that :/', ephemeral: true });
+        if(!member.bannable) return interaction.reply({ content: 'This user cant be unbanned. It is either because they are a mod/admin, or their highest role is higher than mine', ephemeral: true });
+		if(member.user.username === interaction.user.username) return interaction.reply({ content: "You cannot unban yourself!", ephemeral: true });
+
+        member.unban(`${reason}`).catch(error => {
+			console.error(error);
+			interaction.reply({ content: 'There was an error trying to unban this member!', ephemeral: true });
+		});
+
+		return interaction.reply({ 
+			content: `The member ${member.user.username} has been unbanned by ${interaction.user.username} (Reason: ${reason})`, 
+			ephemeral: true 
+		});
+	},
+};
